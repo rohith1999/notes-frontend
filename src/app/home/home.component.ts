d93c01f8e3aece2fd6a7f29601667616f0030eb0
rohith1999 <rohithsuri3@gmail.com>
2022-07-30 12:47:37 +0530

    UI changes and autosubmit feature added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    console.log(this.router.url);
+
   }
 
   onSubmit(){
--- src/app/note-type/note-type.component.ts
+++ src/app/note-type/note-type.component.ts
@@ -1,55 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NoteService } from '../note.service';
-import {Note} from '../note';
+import { Note } from '../note';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  fromEvent,
+  map,
+  Observable,
+} from 'rxjs';
 
 @Component({
   selector: 'app-note-type',
   templateUrl: './note-type.component.html',
-  styleUrls: ['./note-type.component.css']
+  styleUrls: ['./note-type.component.css'],
 })
 export class NoteTypeComponent implements OnInit {
+  timeout: any = null;
 
-  note:Note={
-      noteId: "",
-      noteData: ""
+  note: Note = {
+    noteId: '',
+    noteData: '',
   };
-  
-  isDivVisible=false;
-  
-  constructor(private route: ActivatedRoute, private noteService: NoteService) { }
+
+  isDivVisible = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private noteService: NoteService
+  ) {}
 
   ngOnInit(): void {
-
-      this.route.paramMap.subscribe(paramMap =>{
-
-        this.note.noteId=String(paramMap.get('note_id'));
-        this.getNote(this.note.noteId);
-
-      
-      });
-
-  }
-
-
-  private createNote( note: Note){
-    this.noteService.createNote(note).subscribe(data =>{
-      this.note.noteData=data.noteData;
-      this.isDivVisible=true;
-      setTimeout(() => {
-        this.isDivVisible=false;
-    }, 3000);
+    this.route.paramMap.subscribe((paramMap) => {
+      this.note.noteId = String(paramMap.get('note_id'));
+      this.getNote(this.note.noteId);
     });
   }
 
-  private getNote(noteId: String){
-    this.noteService.getNote(noteId).subscribe(data=>{
-      this.note.noteData=data.noteData;
+  private createNote(note: Note) {
+    this.noteService.createNote(note).subscribe((data) => {
+      this.note.noteData = data.noteData;
+      //   this.isDivVisible=true;
+      //   setTimeout(() => {
+      //     this.isDivVisible=false;
+      // }, 3000);
     });
   }
 
-  onSubmit(){
+  private getNote(noteId: String) {
+    this.noteService.getNote(noteId).subscribe((data) => {
+      this.note.noteData = data.noteData;
+    });
+  }
+
+  // onSubmit(){
+  //   this.createNote(this.note);
+  // }
+
+  noteChanged(event: any) {
+    // setTimeout(() => {
+
     this.createNote(this.note);
+    console.log('call made');
+    // }, 2000);
   }
 
+  onKeySearch(event: any) {
+    clearTimeout(this.timeout);
+    var $this = this;
+    this.timeout = setTimeout(function () {
+      if (event.keyCode != 13) {
+        $this.createNote($this.note);
+        // console.log("api call made");
+      }
+    }, 1000);
+  }
 }
